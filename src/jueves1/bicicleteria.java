1b9023f20e362c5f8ec8d4779267813f01a4ffc8
Desarrollo Web <Desarrollo Web@DESKTOP-CO6LR15>
2019-08-01 16:54:30 -0300

    jueves1

--- src/jueves1/bicicleteria.java
+++ src/jueves1/bicicleteria.java
@@ -1,12 +1,10 @@
 package jueves1;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
 
 public class bicicleteria {
 
-    private List<bicicleta> bici;
+    private ArrayList<bicicleta> bici;
     private float ganancias = 0;
     private Integer cantidadDeVentas = 0;
 
@@ -23,14 +21,14 @@ public class bicicleteria {
     public void venderBicicleta(bicicleta bicicletavendida) {
         bici.remove(bicicletavendida);
         cantidadDeVentas++;
-        ganancias = ganancias+bicicletavendida.getPrecio();
+        ganancias = ganancias + bicicletavendida.getPrecio();
     }
 
     public void buscarBici(String nroDeSerie) {
         boolean encontrado = false;
         for (int i = 0; i < bici.size(); i++) {
             if (bici.get(i).getNroDeSerie().equalsIgnoreCase(nroDeSerie)) {
-                System.out.println("El numero de serie de su bicicleta es: " + bici.get(i).getNroDeSerie());
+                System.out.println("El numero de serie de su bicicleta es: " + bici.get(i));
                 encontrado = true;
             }
         }
@@ -39,13 +37,25 @@ public class bicicleteria {
         }
     }
 
-    public void mostrar() {
-        for (int i = 0; i <bici.size(); i++) {
-            if (bici.get(i).getAño()<bici.get(i).getAño()) {
-                
+    ArrayList<bicicleta> ordenar() {
+        bicicleta auxiliar;
+        boolean qsy = true;
+        for (int j = 0; j < bici.size() && qsy==true ; j++) {
+            qsy = false;
+            for (int i = 0; i < bici.size()-i-j; i++) {
+                if (bici.get(i).getAño() > bici.get(i + 1).getAño()) {
+                    auxiliar = bici.get(i + 1);
+                    bici.set(i + 1, bici.get(i));
+                    bici.set(i, auxiliar);
+                    qsy = true;
+                }
             }
         }
-            System.out.println("" + bici.size());
-        
+        return bici;
+    }
+
+    public void mostrar() {
+        System.out.println(bici.toString());
+
     }
 }
